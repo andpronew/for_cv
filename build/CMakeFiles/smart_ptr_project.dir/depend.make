55684899f47bde2038ae346138346f1bced3a2cf
andpronew <giskoandrey@gmail.com>
2025-11-06 00:25:39 +0200

    Clean root: keep project only under smart_ptr_project/

--- build/CMakeFiles/smart_ptr_project.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for smart_ptr_project.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/big/study/for_portfolio/smart_ptr_project
-# Build directory: /mnt/big/study/for_portfolio/smart_ptr_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/big/study/for_portfolio/smart_ptr_project/build/test_ptr[1]_include.cmake")
