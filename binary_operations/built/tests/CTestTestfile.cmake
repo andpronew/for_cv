e1b8ddd96de83d199a2049c48f11e9d0c46c5a27
andpronew <giskoandrey@gmail.com>
2025-11-11 19:17:29 +0200

    Remove built folder

--- binary_operations/built/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/big/study/binary_operations/tests
-# Build directory: /mnt/big/study/binary_operations/built/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_binary_tests "/mnt/big/study/binary_operations/built/tests/my_binary_tests")
-set_tests_properties(my_binary_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/big/study/binary_operations/tests/CMakeLists.txt;18;add_test;/mnt/big/study/binary_operations/tests/CMakeLists.txt;0;")
